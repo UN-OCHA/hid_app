19562733926ff0da65e0ace4f1d2fd566ea5086e
Joe Turgeon <jturgeon@phase2technology.com>
2015-03-02 13:55:01 -0600

    UNCID-419: Disallow editors from assigning or unassigning roles in the app interface.

--- src/js/app.js
+++ src/js/app.js
@@ -458,7 +458,7 @@ app.controller("ProfileCtrl", function($scope, $location, $route, $routeParams, 
         ||  (checkinFlow && (hasRoleManager || hasRoleEditor))
       );
 
-  $scope.userCanEditRoles = $scope.userCanViewAllFields && profileData.profile._id !== userData.profile._id;
+  $scope.userCanEditRoles = (hasRoleAdmin || hasRoleManager) && profileData.profile._id !== userData.profile._id;
   if ($scope.userCanEditRoles) {
     if (profileService.hasRole('admin', null, profileData) && !hasRoleAdmin) {
       $scope.userCanEditRoles = false;
@@ -466,10 +466,8 @@ app.controller("ProfileCtrl", function($scope, $location, $route, $routeParams, 
     if (profileService.hasRole('manager', null, profileData) && !(hasRoleAdmin || hasRoleManager)) {
       $scope.userCanEditRoles = false;
     }
-    if (profileService.hasRole('editor', null, profileData) && !(hasRoleAdmin || hasRoleManager || hasRoleEditor)) {
-      $scope.userCanEditRoles = false;
-    }
   }
+
   $scope.userCanEditKeyContact = (
             hasRoleAdmin
         ||  (checkinFlow && hasRoleManager)
@@ -479,7 +477,7 @@ app.controller("ProfileCtrl", function($scope, $location, $route, $routeParams, 
 
   // Determine what roles are available to assign to a user
   if ($scope.userCanEditRoles && userData.profile.roles.indexOf('admin') > -1) {
-    // Your an admin and can assign any role
+    // You're an admin and can assign any role
     $scope.adminRoleOptions = roles;
   }
   else if ($scope.userCanEditRoles) {
