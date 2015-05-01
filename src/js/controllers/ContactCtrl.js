bca9a90f12c72ca8b9d9240e4e14121bd5976212
Tim Martin <tmartin@phase2technology.com>
2015-04-30 19:07:20 -0500

    Fixed check out issue

--- src/js/controllers/ContactCtrl.js
+++ src/js/controllers/ContactCtrl.js
@@ -83,8 +83,8 @@ function ContactCtrl($scope, $route, $routeParams, $filter, profileService, gett
   $scope.checkout = function (cid) {
     var contact = {
       _id: $scope.contact._id,
-      _profile: $scope.contact._profile,
-      userid: $scope.contact._profile,
+      _profile: $scope.contact.profile._id,
+      userid: $scope.contact.profile._id,
       status: 0
     };
     if (!$scope.userCanCheckOut) {
