25c4f18102b5bbbb9af98085c82f4f0863e1e148
Joe Turgeon <jturgeon@phase2technology.com>
2015-05-08 16:11:31 -0500

    Switching to use humanitarianresponse.info instead of the HR.info dev site http://hid.hrinfo.568elmp02.blackmesh.com.

--- src/js/config.dev.js
+++ src/js/config.dev.js
@@ -4,6 +4,6 @@ var contactsId = {
     "appBaseUrl": "http://dev.app.568elmp02.blackmesh.com",
     "authBaseUrl": "http://dev.auth.568elmp02.blackmesh.com",
     "profilesBaseUrl": "http://dev.profiles.568elmp02.blackmesh.com",
-    "hrinfoBaseUrl": "http://hid.hrinfo.568elmp02.blackmesh.com",
+    "hrinfoBaseUrl": "https://www.humanitarianresponse.info",
     "googlePlusApiKey": "AIzaSyBI3vq4vutsK_Lic5RrKu4omWF9IS1SaGk"
   };
