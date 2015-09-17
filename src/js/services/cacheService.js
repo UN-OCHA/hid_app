f8448d98c0cc0327f0578ecf882221fa7e9fa55f
Hayyaan Ahmad <hayyaan@gmail.com>
2015-09-17 12:13:05 +0200

    Force Offline fallback on cache miss

--- src/js/services/cacheService.js
+++ src/js/services/cacheService.js
@@ -43,7 +43,7 @@
               return cacheData;
             }
             else {
-              handleError(response);
+              location.replace('#/offline'); //redirect to offline
             }
           }, function(res) {
             handleError(response);
