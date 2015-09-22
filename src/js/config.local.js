111a888215d6c761da0ed9c55156f48e26bf7ece
Guillaume Viguier <guillaume@viguierjust.com>
2015-09-22 12:18:18 +0200

    UNCID-789: added fontawesome-webfont.woff2

--- src/js/config.local.js
+++ src/js/config.local.js
@@ -4,13 +4,13 @@ var contactsId = {
     "appBaseUrl": "http://app.contactsid.vm",
 // The following lines configure HID App to use the *dev* auth, profiles, and
 // HR.info instance.
-    "authBaseUrl": "http://auth.dev.humanitarian.id",
-    "profilesBaseUrl": "http://profiles.dev.humanitarian.id",
+//    "authBaseUrl": "http://auth.dev.humanitarian.id",
+//    "profilesBaseUrl": "http://profiles.dev.humanitarian.id",
     "hrinfoBaseUrl": "https://www.humanitarianresponse.info",
 // The following lines configure HID App to use the local auth, profiles, and
 // HR.info instance.
-//    "authBaseUrl": "http://auth.contactsid.vm",
-//    "profilesBaseUrl": "http://profiles.contactsid.vm",
+    "authBaseUrl": "http://auth.contactsid.vm",
+    "profilesBaseUrl": "http://profiles.contactsid.vm",
 //    "hrinfoBaseUrl": "http://hrinfo.local"
     "googlePlusApiKey": "AIzaSyBI3vq4vutsK_Lic5RrKu4omWF9IS1SaGk"
   };
