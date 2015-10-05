520f199256da2cb6c560a18b2efb20690c12d97a
Guillaume Viguier <guillaume@viguierjust.com>
2015-10-05 10:05:48 +0200

    UNCID-784: initial work

--- src/js/controllers/CustomListSettingsCtrl.js
+++ src/js/controllers/CustomListSettingsCtrl.js
@@ -6,6 +6,41 @@ function CustomListSettingsCtrl($scope, $route, $location, profileService, list,
   $scope.contactsCount = list.contacts.length;
   $scope.path = contactsId.appBaseUrl + '/#/list/contacts?id=' + list._id;
 
+  $scope.refreshReaders = function(select, lengthReq) {
+    var helpOption = {action:'clear', name:"", alt: gettextCatalog.getString('Search term must be at least 3 characters long.'), disable: true},
+        emptyOption = {action:'clear', name:"", alt: gettextCatalog.getString('No results found.'), disable: true};
+
+    // Remove text in parentheses.
+    select.search = select.search.replace(/ *\([^)]*\) */g, "");
+
+    if (select.search.length > (lengthReq || 0)) {
+      select.searching = true;
+
+      $http.get($scope.hrinfoBaseUrl + '/hid/organizations/extended/autocomplete/' + encodeURIComponent(select.search))
+        .then(function(response) {
+          select.searching = false;
+          $scope.organizations = [];
+          angular.forEach(response.data, function(value, key) {
+            this.push({
+              'name': value.name,
+              'org_type_name': value.org_type_name,
+              'org_type_remote_id': value.org_type_id,
+              'remote_id': key
+            });
+          }, $scope.organizations);
+
+          if (!$scope.organizations.length) {
+            $scope.organizations.push(emptyOption);
+          }
+        });
+    }
+    else {
+      $scope.organizations = []
+      $scope.organizations.push(helpOption);
+    }
+  };
+
+
   $scope.saveList = function() {
     // Replace contacts with id instead of object.
     var contacts = [];
