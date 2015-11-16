2434ee03624cbc84410eac89ec9040e914d1d007
Guillaume Viguier <guillaume@viguierjust.com>
2015-11-16 11:10:08 +0100

    UNCID-875: improved edit page and added mailchimp lists selection

--- src/js/controllers/ServicesCtrl.js
+++ src/js/controllers/ServicesCtrl.js
@@ -1,40 +1,60 @@
 function ServicesCtrl($scope, $location, $route, $routeParams, profileService, userData, service) {
 
   $scope.service = service;
+  $scope.mc_lists = [];
+  $scope.alerts = [];
 
   $scope.saveService = function() {
     if (!$scope.service.userid) {
       $scope.service.userid = userData.profile.userid;
     }
-    profileService.saveService($scope.service).then(function(response) {
+    profileService.saveService($scope.service).then(function(response) {  
       if (response.status == 201) {
         $location.path('/services/' + response.data._id + '/edit');
       }
       else if (response.status == 200) {
-        $location.path('/dashboard');
+        $scope.service = response.data;
+        $scope.addAlert('success', 'Service saved successfully.');
       }
-      else {
-        alert('An error occurred while saving this service. Please reload and try the change again.');
-      }
+    }, function (message) {
+      $scope.addAlert('danger', message);
     });
   }
 
   $scope.deleteService = function() {
     profileService.deleteService($scope.service).then(function (response) {
       if (response.status == 204) {
-        $location.path('/dashboard');
+        $scope.addAlert('success', 'Service deleted successfully.');
       }
-      else {
-        alert('An error occurred when trying to delete this service.');
-      }
+    }, function (message) {
+      $scope.addAlert('danger', message);
     });
   }
 
   $scope.getMailchimpLists = function() {
     profileService.getMailchimpLists($scope.service.mc_api_key).then(function (response) {
-      console.log(response);
+      if (response.data.data) {
+        $scope.mc_lists = response.data.data.map(function (val) {
+            return { id: val.id, name: val.name};
+          });
+      }
+    }, function (message) {
+      $scope.alerts.push({type: 'danger', msg: message});
     });
   }
+
+  if ($scope.service.mc_api_key) {
+    $scope.getMailchimpLists();
+  }
+
+  $scope.addAlert = function(type, message) {
+    $scope.alerts.length = 0;
+    $scope.alerts.push({type: type, msg: message});
+  };
+
+  $scope.closeAlert = function(index) {
+    $scope.alerts.splice(index, 1);
+  };
 }
 
 function ServicesListCtrl($scope, $route, $routeParams, profileService, userData) {
