a4422c826cf7e2ad2b83f9f1c023b196d2582b3e
Guillaume Viguier <guillaume@viguierjust.com>
2016-04-19 11:57:12 +0200

    HID-1021 hide youtube video for small screens

--- src/js/controllers/DefaultCtrl.js
+++ src/js/controllers/DefaultCtrl.js
@@ -1,6 +1,10 @@
-function DefaultCtrl($scope, $location, $window, authService) {
+function DefaultCtrl($scope, $location, $window, $sce, authService) {
   $scope.isCollapsed = true;
   $scope.showIframe = $window.window.innerWidth > 460 ? true : false;
+  $scope.iframeCode = '';
+  if ($scope.showIframe) {
+    $scope.iframeCode = $sce.trustAsHtml('<iframe id="video" src="https://www.youtube.com/embed/tqBwJld2Sz8?rel=0&controls=0&showinfo=0" frameborder="0" allowfullscreen></iframe>');
+  }
 
   function parseLocation(location) {
     var pairs = location.substring(1).split("&"),
