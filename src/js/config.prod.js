507ad87c87666a8dbba60ae0dfa5bb80a8f1519b
Guillaume Viguier <guillaume@viguierjust.com>
2017-04-07 08:58:01 +0200

    Changed prod url

--- src/js/config.prod.js
+++ src/js/config.prod.js
@@ -1,8 +1,8 @@
 var contactsId = {
     "sourcePath": "",
     "authClientId": "hid-prod",
-    "appBaseUrl": "https://humanitarian.id",
-    "authBaseUrl": "https://auth.humanitarian.id",
+    "appBaseUrl": "https://app1.humanitarian.id",
+    "authBaseUrl": "https://auth1.humanitarian.id",
     "profilesBaseUrl": "https://profiles.humanitarian.id",
     "hrinfoBaseUrl": "https://www.humanitarianresponse.info",
     "googlePlusApiKey": "AIzaSyBJOqcOsd8lS2ifx_FjwGyeKVOKBijCDks",
